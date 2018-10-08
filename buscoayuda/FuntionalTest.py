e4980fc8992b2161c86583a7a123e043c7c46018
Zulma Castañeda <zl.castaneda10@uniandes.edu.co>
2018-10-07 19:36:28 -0500

    Rojo: Test titulo busco ayuda

--- buscoayuda/FuntionalTest.py
+++ buscoayuda/FuntionalTest.py
@@ -6,12 +6,13 @@ import sys
 
 class FunctionalTest(TestCase):
     def setUp(self):
+        #self.browser = webdriver.Chrome('C:/chromedriver.exe')
         self.browser = webdriver.Chrome('C://chromedriver.exe')
 
     def tearDown(self):
         self.browser.quit()
 
     def test_1_title(self):
-        self.browser.get('http://www.google.com.co')
-        self.assertIn('Google', self.browser.title)
+        self.browser.get('http://127.0.0.1:8000/')
+        self.assertIn('Busco Ayuda', self.browser.title)
 
